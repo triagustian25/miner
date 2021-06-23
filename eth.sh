8c92e1b074b300108d16a845b2822fa1b4c47b35
triagustian25 <82904028+triagustian25@users.noreply.github.com>
2021-06-24 04:11:59 +0700

    Update and rename eth.sh to dogecoin.sh

--- eth.sh
+++
@@ -1,4 +0,0 @@
-# eth
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-./xmrig stratum+tcp://ethash.poolbinance.com:1800 -u 0x27fFC79e3335bFE4EC98EA76C25B6175344f190C.TRIAGUSTIAN25.001 -p x --cpu 4
