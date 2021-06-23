e5ad93e3397042c9ebc9e052b881a2fda595009f
triagustian25 <82904028+triagustian25@users.noreply.github.com>
2021-06-24 04:45:00 +0700

    Update dogecoin.sh

--- dogecoin.sh
+++ dogecoin.sh
@@ -1,6 +1,6 @@
 # dogeoin
 sudo apt update
-!wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
-!tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
 %cd xmrig-6.12.2
-!./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DSTAHL2stkr8gacurFvkeTgHnKqBPJ5o5t -p x --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DSTAHL2stkr8gacurFvkeTgHnKqBPJ5o5t -p x --cpu 4
